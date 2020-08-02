74ed10d40aa351fe77896b629440aede04839633
Sandeep Bari <64518228+SandeepBari@users.noreply.github.com>
2020-08-02 17:33:27 +0530

    Randmization Improved

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -12,37 +12,39 @@ passTemp1= " "
 checkFlag= 0;
 
 if entry == '1':
-	numValOri= input('Enter Password Length: ')
+	numVal= input('Enter Password Length: ')
 	print('Enter the Number Range between 0 to 9')
-	lowRangeNum= input("Enter lowest range number: ")
-	highRangeNum= input("Enter highest range number: ")
+	lowRangeNum= input("Enter lowest range of number: ")
+	highRangeNum= input("Enter highest range of number: ")
 
 	if lowRangeNum < highRangeNum:
 		StringTemp= input("Enter the characters you want: ")
 
-		for i in range(int(numValOri)):
+		for i in range(int(numVal)):
 			num= str(random.randint(int(lowRangeNum), int(highRangeNum)))
 			alpha= str(random.choice(StringTemp))
 			passTemp1 = passTemp1+ alpha+ num 
 
-		passTemp1= passTemp1[1:int(numValOri)+1]
-		print('Your Password: '+ passTemp1)          
+		passTemp= passTemp1[1:int(numVal)+1]
+		print('Your Password: '+ passTemp)
+		checkFlag= 0          
 
 	else:
 		print("Please Enter Proper Range")
 		checkFlag= 1
 
 elif (entry == '2'):
-	numValOri= input('Enter Password Length: ')
+	numVal= input('Enter Password Length: ')
 
-	for i in range(int(numValOri)):
+	for i in range(int(numVal)):
 		num= str(random.randint(0,9))
 		alphaUpper= str(random.choice(string.ascii_uppercase))
 		alphaLower= str(random.choice(string.ascii_lowercase))
 		passTemp1 = passTemp1+ alphaUpper+ alphaLower+ num
   
-	passTemp1= passTemp1[1:int(numValOri)+1]
-	print('Your Password: '+ passTemp1)
+	passTemp= passTemp1[1:int(numVal)+1]
+	print('Your Password: '+ passTemp)
+	checkFlag= 0
    
 else:
 	print("Please Select Proper Mode Of Generating Password")
@@ -55,12 +57,12 @@ if checkFlag == 0:
 	if check == 'y' or check == 'Y':
 		password= " "
 
-		for i in range(int(numValOri)):
+		for i in range(int(numVal)):
 			passTemp2 = random.choice(passTemp1)
 			password = password + passTemp2
 
 		print(password)
-		passTemp1= password;
+		passTemp= password;
 
 if path.exists("Password.txt"):
 	os.remove("Password.txt")
@@ -68,6 +70,6 @@ if path.exists("Password.txt"):
 datetimeVal = str(datetime.now())
 f = open("Password.txt", "a")
 f.write("Your Password is: ")
-f.write(passTemp1)
+f.write(passTemp)
 f.write("\n"+ datetimeVal)
 f.close()		
